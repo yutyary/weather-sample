387c49b11a64d0a2b93e830187ec1ceb5501d2a4
もっちー <yutyary@gmail.com>
2020-12-30 15:47:51 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    item += 1
+    item = !(item)
 })
-let item = 0
-weatherbit.startWeatherMonitoring()
+let current_WindDirection_List = ""
+let current_WindSpeed = 0
+let item = false
+serial.redirectToUSB()
+weatherbit.startWindMonitoring()
+item = true
+/**
+ * Note: If "???" is displayed, direction is unknown!
+ */
 basic.forever(function () {
-    if (item == 0) {
-        basic.showString("Temp C: ")
-        basic.showNumber(Math.idiv(weatherbit.temperature(), 100))
-    } else if (item == 1) {
-        basic.showString("Humidity %: ")
-        basic.showNumber(Math.idiv(weatherbit.humidity(), 1024))
-    } else if (item == 2) {
-        basic.showString("Pressure hPa: ")
-        basic.showNumber(Math.idiv(weatherbit.pressure(), 25600))
+    current_WindSpeed = weatherbit.windSpeed()
+    current_WindDirection_List = weatherbit.windDirection()
+    if (item) {
+        basic.showString("Sp")
+        basic.showNumber(Math.trunc(current_WindSpeed))
     } else {
-        item = 0
+        basic.showString("Dir")
+        basic.showString(current_WindDirection_List)
     }
+    serial.writeLine("" + current_WindSpeed + "," + current_WindDirection_List)
 })
