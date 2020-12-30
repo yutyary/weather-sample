2ff09b3323b694d848ea20274331a9615986847e
もっちー <yutyary@gmail.com>
2020-12-30 14:11:24 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    item = !(item)
+})
+let current_WindDirection_List = ""
+let current_WindSpeed = 0
+let item = false
+serial.redirectToUSB()
+weatherbit.startWindMonitoring()
+item = true
+// Note: If "???" is displayed, direction is unknown!
+basic.forever(function () {
+    current_WindSpeed = weatherbit.windSpeed()
+    current_WindDirection_List = weatherbit.windDirection()
+    if (item) {
+        basic.showString("Sp")
+        basic.showNumber(Math.trunc(current_WindSpeed))
+    } else {
+        basic.showString("Dir")
+        basic.showString(current_WindDirection_List)
+    }
+    serial.writeLine("" + current_WindSpeed + "," + current_WindDirection_List)
+})
